069ba9ea90b79ee7e3b8c06fadd5de66b5ec7def
Andrei Kamarouski <akamarouski@solvd.com>
2023-09-11 10:17:32 +0200

    refactor(ZebrunnerProxyBuilder): remove unused header

--- src/main/java/com/zebrunner/carina/webdriver/proxy/ZebrunnerProxyBuilder.java
+++ src/main/java/com/zebrunner/carina/webdriver/proxy/ZebrunnerProxyBuilder.java
@@ -3,7 +3,6 @@ package com.zebrunner.carina.webdriver.proxy;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import javax.annotation.Nonnull;
