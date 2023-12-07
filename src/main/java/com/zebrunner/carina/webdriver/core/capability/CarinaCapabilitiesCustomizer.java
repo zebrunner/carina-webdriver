41542f5830a7cb6f06a29a269f1ee4a9c2ef715f
Andrei Kamarouski <akamarouski@solvd.com>
2023-12-07 20:45:23 +0100

    fix(CarinaCapabilitiesCustomizer): add zebrunner prefix for legacy capabilities

--- src/main/java/com/zebrunner/carina/webdriver/core/capability/CarinaCapabilitiesCustomizer.java
+++ src/main/java/com/zebrunner/carina/webdriver/core/capability/CarinaCapabilitiesCustomizer.java
@@ -1,8 +1,12 @@
 package com.zebrunner.carina.webdriver.core.capability;
 
 import com.zebrunner.agent.core.webdriver.CapabilitiesCustomizer;
+import org.apache.commons.lang3.StringUtils;
 import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.MutableCapabilities;
+
+import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * Hotfix for 'legacy' capabilities that fail session
@@ -12,9 +16,18 @@ public class CarinaCapabilitiesCustomizer implements CapabilitiesCustomizer {
 
     @Override
     public Capabilities customize(Capabilities capabilities) {
-        if (capabilities.getCapability("provider") != null) {
-            ((MutableCapabilities) capabilities).setCapability("provider", (String) null);
+        Set<String> capabilitiesMap = capabilities.asMap().keySet()
+                .stream()
+                .filter(key -> StringUtils.equalsAnyIgnoreCase(key, "provider", "idleTimeout", "enableVNC", "enableLog", "enableVideo", "cpu",
+                        "memory"))
+                .collect(Collectors.toSet());
+
+        MutableCapabilities mutableCapabilities = (MutableCapabilities) capabilities;
+        for (String key : capabilitiesMap) {
+            Object value = mutableCapabilities.getCapability(key);
+            mutableCapabilities.setCapability(key, (String) null);
+            mutableCapabilities.setCapability("zebrunner:" + key, value);
         }
-        return capabilities;
+        return mutableCapabilities;
     }
 }
