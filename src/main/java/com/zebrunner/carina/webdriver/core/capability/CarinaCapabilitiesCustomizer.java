9fbdfe0e5b12b9fee9f17b4a7f7c71a5f3f60b6d
Andrei Kamarouski <akamarouski@solvd.com>
2024-01-18 18:32:51 +0100

    feature: customize more zebrunner capabilities

--- src/main/java/com/zebrunner/carina/webdriver/core/capability/CarinaCapabilitiesCustomizer.java
+++ src/main/java/com/zebrunner/carina/webdriver/core/capability/CarinaCapabilitiesCustomizer.java
@@ -1,6 +1,7 @@
 package com.zebrunner.carina.webdriver.core.capability;
 
 import com.zebrunner.agent.core.webdriver.CapabilitiesCustomizer;
+import org.apache.commons.lang3.StringUtils;
 import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.MutableCapabilities;
 
@@ -15,6 +16,15 @@ public class CarinaCapabilitiesCustomizer implements CapabilitiesCustomizer {
         if (capabilities.getCapability("provider") != null) {
             ((MutableCapabilities) capabilities).setCapability("provider", (String) null);
         }
+        capabilities.getCapabilityNames()
+                .stream()
+                .filter(name -> StringUtils.equalsIgnoreCase("zeb-applicationType", name))
+                .findFirst()
+                .ifPresent((name -> {
+                    Object value = capabilities.getCapability(name);
+                    ((MutableCapabilities) capabilities).setCapability(name, (String) null);
+                    ((MutableCapabilities) capabilities).setCapability("zebrunner:" + name, value);
+                }));
         return capabilities;
     }
 }
